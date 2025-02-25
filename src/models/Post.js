3025247ce257b561b831f68bf68050fb304d011c
Mohsen barahuee <nakel3920@gmail.com>
2025-02-25 18:30:11 +0330

    create post model

---
+++ src/models/Post.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    media: {
+        path: { type: String, required: true },
+        filename: { type: String, required: true },
+    },
+    desription: {
+        type: String
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    }
+})
+
+
+const model = mongoose.model("Post", schema)
+
+module.exports = model
