41a67d5c525d84b104a377099aa46ae7efbdfa35
Mohsen barahuee <nakel3920@gmail.com>
2025-02-25 18:21:16 +0330

    ss

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,7 +1,9 @@
 const userModel = require("../../models/user")
+const RefreshToken = require('../../models/RefreshToken')
 const { errorResponse, successResponse } = require("../../utils/response")
 const { registerValidationSchema } = require('./auth.validator')
-
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 exports.register = async (req, res, next) => {
 
@@ -32,6 +34,22 @@ exports.register = async (req, res, next) => {
         let user = new userModel({ email, username, password, name })
         user = await user.save()
 
+        const accessToken = jwt.sign({ userID: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "30day"
+        })
+
+        const refreshToken = await RefreshToken.createToken(user)
+
+
+        res.cookie("token", accessToken, {
+            maxAge: 900_000,
+            httpOnly: true
+        })
+        res.cookie("refreshToken", refreshToken, {
+            maxAge: 900_000,
+            httpOnly: true
+        })
+
         return successResponse(res, 201, {
             message: "User Created Successfully :)) ",
             user: { ...user.toObject(), password: undefined }
@@ -49,3 +67,59 @@ exports.showRegisterView = async (req, res) => {
     return res.render('auth/register')
 
 }
+
+
+exports.showLoginView = async (req, res) => {
+
+    return res.render('auth/login')
+
+}
+exports.login = async (req, res, next) => {
+
+    try {
+        const { email, password } = req.body
+
+        // validation
+
+        const user = await userModel.findOne({ email }).lean()
+
+        if (!user) {
+            return errorResponse(res, 400, "Email Or Username is not correct !!")
+        }
+
+        const isPasswordCorrct = await bcrypt.compare(password, user.password)
+        if (!isPasswordCorrct) {
+            return errorResponse(res, 400, "password is not correct !!")
+        }
+        
+        const accessToken = jwt.sign({ userID: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "30day"
+        })
+
+        const refreshToken = await RefreshToken.createToken(user)
+
+
+        res.cookie("token", accessToken, {
+            maxAge: 900_000,
+            httpOnly: true
+        })
+        res.cookie("refreshToken", refreshToken, {
+            maxAge: 900_000,
+            httpOnly: true
+        })
+
+        return successResponse(res, 201, {
+            message: "Login is successfull :)) ",
+            user: { ...user.toObject(), password: undefined }
+        })
+
+
+
+
+    } catch (error) {
+        next()
+    }
+
+}
+
+
--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 
 router.route('/register').get(controller.showRegisterView).post(controller.register)
+router.route('/login').get(controller.showLoginView).post(controller.login)
 
 module.exports = router
 
